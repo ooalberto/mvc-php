51ca8b07356bcbee850db83e24a4cb92ebdb66f8
= <=>
2021-08-25 23:08:41 -0300

    com abstracao

--- App/route.php
+++ App/route.php
@@ -2,17 +2,12 @@
 
 namespace App;
 
-class Route
+use BIRT\init\Bootstrap;
+
+class Route extends Bootstrap
 {
-    private $routes;
 
-    public function __construct()
-    {
-        $this->initRoutes();
-        $this->run($this->getUrl());
-    }
-
-    public function initRoutes()
+    protected function initRoutes()
     {
         $routes['home'] = array(
             'route' => '/',
@@ -26,29 +21,5 @@ class Route
             'action' => 'contact'
         );
         $this->setRoutes($routes);
-    }
-
-    public function run($url)
-    {
-        array_walk($this->routes, function ($route) use ($url) {
-            if ($url == $route['route']) {
-                $class = "App\\Controllers\\" . ucfirst($route['controller']);
-                // $class  ='teste';
-                $controller = new $class;
-                $action = $route['action'];
-                $controller->$action();
-            }
-        });
-    }
-
-    public function setRoutes(array $routes)
-    {
-        $this->routes = $routes;
-    }
-
-    public function getUrl()
-    {
-        # return 'testeasffs';
-        return parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
     }
 }

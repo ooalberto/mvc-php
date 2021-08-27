856787fd223761e89f9b18629b0a79bfacf46e27
= <=>
2021-08-26 22:10:01 -0300

    renderizando ao view

--- App/Controllers/IndexController.php
+++ App/Controllers/IndexController.php
@@ -4,16 +4,33 @@ namespace App\Controllers;
 
 class IndexController
 {
+    private $view;
+
+    public function __construct()
+    {
+        $this->view = new \stdClass;
+    }
     public function index()
     {
-        $cars = array("Mustang","Ferrari");
-        include_once "../App/Views/index.phtml";
-    }
+        $this->view->cars = array("Mustang","Ferrari");
+        // include_once "../App/Views/index/index.phtml";
+        $this->render("index");
+    } 
 
     public function contact()
     {
-        $cars = array("Mustang","Ferrari");
-        include_once "../App/Views/index.phtml";
+        $this->view->cars = array("Mustang","Ferrari");
+        // include_once "../App/Views/index/contact.phtml";
+        $this->render("contact");
     }
+
+    public function render($action)
+    {
+        $current = get_class($this);
+        $singleCassName = strtolower(str_replace("Controller", "", str_replace("App\\Controllers\\", "", $current)));
+
+        include_once "../App/Views/" . $singleCassName . "/" . $action . ".phtml";
+    }
+    
     
 }

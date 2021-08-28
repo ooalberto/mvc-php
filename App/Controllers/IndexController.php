ca24276e481eebe78976c2a2ba8e59f31c9f0ca1
= <=>
2021-08-28 19:43:53 -0300

    add layout

--- App/Controllers/IndexController.php
+++ App/Controllers/IndexController.php
@@ -4,14 +4,14 @@ namespace App\Controllers;
 
 use BIRT\Controller\Action;
 
-class IndexController extends Action
+class IndexController extends Action    
 {
    
     public function index()
     {
         $this->view->cars = array("Mustang","Ferrari");
         // include_once "../App/Views/index/index.phtml";
-        $this->render("index");
+        $this->render("index",true);
     } 
 
     public function contact()
--- vendor/BIRT/Controller/Action.php
+++ vendor/BIRT/Controller/Action.php
@@ -12,12 +12,20 @@ abstract class Action
         $this->view = new \stdClass;
     }
 
-    protected function render($action)
+    protected function render($action, $layout = true)
     {
         $this->action = $action;
+        if ($layout == true && file_exists('../App/Views/layout.phtml')) {
+            include_once '../App/Views/layout.phtml';
+        } else {
+            $this->content();
+        }
+    }
+
+    protected function content()
+    {
         $current = get_class($this);
         $singleCassName = strtolower(str_replace("Controller", "", str_replace("App\\Controllers\\", "", $current)));
-
         include_once "../App/Views/" . $singleCassName . "/" .  $this->action . ".phtml";
     }
 }
